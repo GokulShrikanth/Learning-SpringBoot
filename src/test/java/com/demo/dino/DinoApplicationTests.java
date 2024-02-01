c7fd34b1b6ed012a084af8bdbd127d1929db65de
Gokul Shrikanth <gokulshrikanth99@gmail.com>
2024-02-01 16:27:19 +0000

    inital commit

---
+++ src/test/java/com/demo/dino/DinoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.dino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
